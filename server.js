93b8b569540af25c87c38c609d0a129fc0084dc4
xuannhi95 <nhick21v7k025@vlvh.ctu.edu.vn>
2022-10-08 13:49:22 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const app = require ("./app");
-const config = require ("./app/config");
-    //start server
-    const PORT = config.app.port;
-    app.listen(PORT, () => {
-        console.log ('Server is running on port $ {PORT}.');
-    });
+const app = require("./app");
+const config = require("./app/config");
+
+// start server
+const PORT = config.app.port;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
